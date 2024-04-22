45d9a50c82b45bd71a256678b3259ed76caccbd1
armin-164 <arminsadr164@hotmail.com>
2024-04-22 22:01:47 +0200

    improvement(script.js): change duration for animation

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const classArray = [".l-1", ".l-2", ".l-3", ".l-4", ".l-5"]
 
 function activateAnimation() {
     classArray.forEach(letter => {
-        tl.to(letter, 2, {strokeDashoffset:"0"});   
+        tl.to(letter, 1, {strokeDashoffset:"0"});   
     })
-    tl.to(".cls-1", 1.2, {fillOpacity:1}, "-=1");
+    tl.to(".cls-1", 2.2, {fillOpacity:1}, "-=0.2");
 
 }
 
